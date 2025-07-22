2740c10d0198abdafbbb20a96148677de536cd9d
itsuki <itsuki.enjyoy@gmail.com>
2025-07-22 14:48:27 +0900

    Initial Commit

---
+++ ScreenCaptureKitDemo/ScreenCaptureKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenCaptureKitDemoApp.swift
+//  ScreenCaptureKitDemo
+//
+//  Created by Itsuki on 2025/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ScreenCaptureKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
